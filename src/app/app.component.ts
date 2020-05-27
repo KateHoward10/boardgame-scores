2e378b2715d2ff4ad0c7af5e2343dc5053df3622
Kate Howard <k.l.howard.1992@gmail.com>
2020-05-27 17:11:32 +0100

    Add player names

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'boardgame-scores';
+
+  points: {
+    'orange': 1,
+    'blue': 2,
+    'green': 3,
+    'yellow': 4,
+    'purple': 5,
+    'dark green': 6,
+    'dark purple': 7,
+    'black': 8,
+    'red': 10
+  };
+  players: any[];
+
+  ngOnInit(): void {
+    this.players = [];
+  }
+
+  addPlayer(name: string) {
+    this.players.push(name);
+  }
 }
